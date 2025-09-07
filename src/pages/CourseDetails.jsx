4fff3ac018aab4502b1265d952f3230df59fb04b
arpitataa <arpitananda267@gmail.com>
2025-09-08 02:09:19 +0530

    done w course page styling ! it is nice to see the pieces finaly assemble together. also next is view subsection page

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { buyCourse } from '../services/operations/studentFeaturesAPI';
+import ReactMarkdown from "react-markdown";
 import { fetchCourseDetails } from '../services/operations/courseDetailsAPI';
-import { setCourse } from '../slices/courseSlice';
 import GetAvgRating from '../utils/avgRating';
 import Error from "./Error"
+
 import ConfirmationModal from "../components/common/ConfirmationModal"
 import RatingStars from "../components/common/RatingStars"
 import { formatDate } from '../services/formatDate';
 
 import CourseDetailsCard from '../components/core/Course/CourseDetailsCard';
+import CourseAccordionBar from '../components/core/Course/CourseAccordionBar';
+import Footer from '../components/common/Footer';
+import { BiInfoCircle } from "react-icons/bi";
+import { HiOutlineGlobeAlt } from "react-icons/hi2";
 
 const CourseDetails = () => {
 
@@ -20,11 +25,14 @@ const CourseDetails = () => {
     const {paymentLoading} = useSelector((state)=> state.course);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
     const {courseId}  = useParams();
 
     const [courseData , setCourseData] = useState(null);
     const [confirmationModal, setConfirmationModal] = useState(null);
+
     useEffect(()=> {
+        //calling getCourse Details
         const getCourseFullDetails = async() => {
             try{
                 const result = await fetchCourseDetails(courseId);
@@ -32,7 +40,7 @@ const CourseDetails = () => {
                 setCourseData(result);
             }
             catch(error) {
-                console.log("Could not fetch coursse details");
+                console.log("Could not fetch course details");
             }
         }
         getCourseFullDetails();
@@ -41,8 +49,9 @@ const CourseDetails = () => {
 
     const [avgReviewCount, setAverageReviewCount] = useState(0);
 
+    //GET AVG RATING COUNT
     useEffect(()=> {
-        const count = GetAvgRating(courseData?.data?.courseDetails.ratingAndReviews);
+        const count = GetAvgRating(courseData?.data?.courseDetails?.ratingAndReviews || []);
         setAverageReviewCount(count);
     },[courseData])
 
@@ -50,7 +59,7 @@ const CourseDetails = () => {
     useEffect(()=> {
         let lectures = 0;
         courseData?.data?.courseDetails?.courseContent?.forEach((sec) => {
-            lectures += sec.SubSection.length || 0
+            lectures += (sec?.SubSection?.length || 0)
         })
         setTotalNoOfLectures(lectures);
 
@@ -86,17 +95,15 @@ const CourseDetails = () => {
 
     if(loading || !courseData) {
         return (
-            <div>
-                Loading...
+            <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+            <div className="spinner"></div>
             </div>
         )
     }
 
     if(!courseData.success) {
         return (
-            <div>
-                <Error />
-            </div>
+            <Error />
         )
     }
     const {
@@ -111,84 +118,158 @@ const CourseDetails = () => {
         Instructor,
         studentsEnrolled,
         createdAt,
-    } = courseData?.data?.courseDetails;
+    } = courseData?.data?.courseDetails || {};
+
+    if (paymentLoading) {
+    // console.log("payment loading")
+    return (
+      <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+        <div className="spinner"></div>
+      </div>
+    )
+  }
 
   return (
-    <div className='flex flex-col  text-white'>
-
-        <div className='relative flex flex-col justify-start p-8'>
-            <p>{courseName}</p>
-            <p>{courseDescription}</p>
-            <div className='flex gap-x-2'>
-                <span>{avgReviewCount}</span>
+    <>
+    <div className='flex flex-col text-white'>
+      <div className='relative w-full bg-richblack-800'>
+        {/* Hero Section */}
+        <div className='mx-auto box-content px-4 lg:w-[1260px] 2xl:relative '>
+          <div className="mx-auto grid min-h-[450px] max-w-maxContentTab justify-items-center py-8 lg:mx-0 lg:justify-items-start lg:py-0 xl:max-w-[810px]">
+            <div className="relative block max-h-[30rem] lg:hidden">
+              <div className="absolute bottom-0 left-0 h-full w-full shadow-[#161D29_0px_-64px_36px_-28px_inset]"></div>
+              <img
+                src={thumbnail}
+                alt="course thumbnail"
+                className="aspect-auto w-full"
+              />
+            </div>
+            <div
+              className={`z-30 my-5 flex flex-col justify-center gap-4 py-5 text-lg text-richblack-5`} >
+              <div>
+                <p className="text-4xl font-bold text-richblack-5 sm:text-[42px]">
+                  {courseName}
+                </p>
+              </div>
+              <p className={`text-richblack-200`}>{courseDescription}</p>
+              <div className="text-md flex flex-wrap items-center gap-2">
+                <span className="text-yellow-25">{avgReviewCount}</span>
                 <RatingStars Review_Count={avgReviewCount} Star_Size={24} />
-                <span>{`(${ratingAndReviews.length} reviews) `}</span>
-                <span>{`(${studentsEnrolled.length} students enrolled)`}</span>
-            </div>
-
-            <div>
-                <p>Created By {`${Instructor.firstName}`}</p>
-            </div>
-
-            <div className='flex gap-x-3'>
-                <p>
-                    Created At {formatDate(createdAt)}
+                <span>{`(${ratingAndReviews?.length || 0} reviews)`}</span>
+                <span>{`${studentsEnrolled?.length || 0} students enrolled`}</span>
+              </div>
+              <div>
+                <p className="">
+                  Created By {`${Instructor?.firstName || ""} ${Instructor?.lastName || ""}`}
                 </p>
-                <p>
-                    {" "} English
+              </div>
+
+              <div className="flex flex-wrap gap-5 text-lg">
+                <p className="flex items-center gap-2">
+                  <BiInfoCircle /> Created at {formatDate(createdAt)}
                 </p>
+                <p className="flex items-center gap-2">
+                  <HiOutlineGlobeAlt /> English
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex w-full flex-col gap-4 border-y border-y-richblack-500 py-4 lg:hidden">
+            <p className="space-x-3 pb-4 text-3xl font-semibold text-richblack-5">
+              Rs. {price}
+            </p>
+            <button className="yellowButton" onClick={handleBuyCourse}>
+              Buy Now
+            </button>
+            <button className="blackButton">Add to Cart</button>
+          </div>
+
+          {/* courses card */}
+          <div className="right-[1rem] top-[60px] mx-auto hidden min-h-[600px] w-1/3 max-w-[410px] translate-y-24 md:translate-y-0 lg:absolute lg:block">
+            <CourseDetailsCard 
+              course = {courseData?.data?.courseDetails}
+              setConfirmationModal = {setConfirmationModal}
+              handleBuyCourse = {handleBuyCourse}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto box-content px-4 text-start text-richblack-5 lg:w-[1260px]" >
+        <div className="mx-auto max-w-maxContentTab lg:mx-0 xl:max-w-[810px]">
+            {/* what you will learn section */}
+            <div className="my-8 border border-richblack-600 p-8">
+                <p className="text-3xl font-semibold">What you'll learn</p>
+                <div className="mt-5">
+                    <ReactMarkdown>{whatYouWillLearn}</ReactMarkdown>
+                </div>
+            </div>
+        
+            {/* Course Content Section */}
+            <div className="max-w-[830px] ">
+                <div className="flex flex-col gap-3">
+                    <p className="text-[28px] font-semibold">Course Content</p>
+                    <div className="flex flex-wrap justify-between gap-2">
+                    <div className="flex gap-2"></div>
+                    <span>
+                    {courseContent?.length || 0} {`section(s)`}
+                  </span>
+                  <span>
+                    {totalNoOfLectures} {`lecture(s)`}
+                  </span>
+                  <span>{courseData?.data?.totalDuration || 0} total length</span>
+                  </div>
+                <div>
+                  <button
+                    className="text-yellow-25"
+                    onClick={() => setIsActive([])}
+                  >
+                    Collapse all sections
+                  </button>
+                </div>
+              </div>
             </div>
 
-            <div>
-                <CourseDetailsCard 
-                    course = {courseData?.data?.courseDetails}
-                    setConfirmationModal = {setConfirmationModal}
-                    handleBuyCourse = {handleBuyCourse}
+        {/* Course Details Accordion */}
+        <div className="py-4">
+            {courseContent?.map((course, index) => (
+            <CourseAccordionBar
+                course={course}
+                key={index}
+                isActive={isActive}
+                handleActive={handleActive}
+            />
+            ))}
+        </div>
+
+        {/* Author Details */}
+        <div className="mb-12 py-4">
+            <p className="text-[28px] font-semibold">Author</p>
+              <div className="flex items-center gap-4 py-4">
+                <img
+                  src={
+                    Instructor?.image
+                      ? Instructor.image
+                      : `https://api.dicebear.com/5.x/initials/svg?seed=${encodeURIComponent(`${Instructor?.firstName || ""} ${Instructor?.lastName || ""}`)}`
+                  }
+                  alt="Author"
+                  className="h-14 w-14 rounded-full object-cover"
                 />
+                <p className="text-lg">{`${Instructor?.firstName || ""} ${Instructor?.lastName || ""}`}</p>
+              </div>
+              <p className="text-richblack-50">
+                {Instructor?.additionalDetails?.about || ""}
+              </p>
             </div>
         </div>
-
-        <div>
-            <p> What You WIll learn</p>
-            <div>
-                {whatYouWillLearn}
-            </div>
-        </div>
-
-        <div>
-            <div>
-                <p>Course Content:</p>
-            </div>
-
-            <div className='flex gap-x-3 justify-between'>
-
-                   <div>
-                    <span>{courseContent?.length || 0} section(s)</span>
-
-                        <span>
-                            {totalNoOfLectures} lectures
-                        </span>
-                        <span>
-                            {courseData.data?.totalDuration || 0} total length
-                        </span>
-                   </div>
-
-                   <div>
-                        <button
-                            onClick={() => setIsActive([])}>
-                            Collapse all Sections
-                        </button>
-                   </div>
-
-            </div>
-        </div>
-
-
-
-
-
-        {confirmationModal && <ConfirmationModal modalData={confirmationModal}/>}
+      </div>
     </div>
+    
+    <Footer />
+    
+        {confirmationModal && <ConfirmationModal modalData={confirmationModal}/>}
+    </>
   )
 }
 
